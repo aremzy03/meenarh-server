452358c330629b81fd39cb913110773b7c40e151
Abdulmalik Aremu <aremzy2018@gmail.com>
2026-03-25 13:27:09 +0100

    logged the process.env.port

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
